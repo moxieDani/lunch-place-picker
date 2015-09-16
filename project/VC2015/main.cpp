f20d1695912c283552274ef88d16aec55eda8301
daniel-kisoon-kwon <moxie2ks@gmail.com>
2015-09-17 02:05:44 +0900

    Created API - add, modify, remove

--- project/VC2015/main.cpp
+++ project/VC2015/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char* argv[])
 {
 	init("lunch-place-info.txt");
+	modify(1, "진순대", "뼈해장국과 순대국을 동시에!");
+	remove(2);
 	exit();
 	return 0;
 }
--- src/LunchPlacePicker.cpp
+++ src/LunchPlacePicker.cpp
@@ -5,12 +5,13 @@
 #include "LunchPlacePicker.h"
 
 FILE *fp;
-static LunchPlaceInfo* gLunchPlace = (LunchPlaceInfo*)malloc(sizeof(LunchPlaceInfo));
+static LunchPlaceInfo* gLunchPlaceHEAD = (LunchPlaceInfo*)malloc(sizeof(LunchPlaceInfo));
+static LunchPlaceInfo* gLunchPlaceTAIL;
 static int total = 0;
 
 int init(char* path)
 {
-	LunchPlaceInfo* LunchPlace = gLunchPlace;
+	LunchPlaceInfo* LunchPlace = gLunchPlaceHEAD;
 	if (NULL == (fp = fopen(path, "r+")))
 	{
 		printf("File open error!\n");
@@ -21,18 +22,19 @@ int init(char* path)
 	while (1)
 	{
 		char* line = getLine();
-		if ( 0 == strlen(line) ) { break; }
+		if (0 == strlen(line)) { break; }
 		setLunchPlaceInfo(line, LunchPlace);
 		free(line);
-		
-		printf("FREQ(%d) NAME(%s) DESC(%s)\n", LunchPlace->frequency, LunchPlace->name, LunchPlace->desc);
+
+		printf("[init] FREQ(%d) NAME(%s) DESC(%s)\n", LunchPlace->frequency, LunchPlace->name, LunchPlace->desc);
+		gLunchPlaceTAIL = LunchPlace;
 		LunchPlace->pNext = (LunchPlaceInfo*)malloc(sizeof(LunchPlaceInfo));
 		LunchPlace = LunchPlace->pNext;
 		LunchPlace->pNext = NULL;
 		total++;
 	}
 	printf("total count : %d\n", total);
-	
+
 	printf("init done\n");
 	return 0;
 }
@@ -45,14 +47,14 @@ static char* getLine()
 	while (1)
 	{
 		fread(&buf, 1, 1, fp);
-		if ( '\n' == buf || feof(fp) ) break;
+		if ('\n' == buf || feof(fp)) break;
 		lineLength++;
 
 		line = (char*)realloc(line, sizeof(char)*lineLength);
 		line[lineLength - 2] = buf;
 	}
 	line[lineLength - 1] = '\0';
-	
+
 	return line;
 }
 
@@ -74,34 +76,117 @@ static void setLunchPlaceInfo(char* line, LunchPlaceInfo* LunchPlace)
 char* pick()
 {
 	srand(time(NULL));
-	
-	LunchPlaceInfo* temp = gLunchPlace;
+
+	LunchPlaceInfo* temp = gLunchPlaceHEAD;
 	return 0;
 }
 
 int add(char* name, char* desc)
 {
 	LunchPlaceInfo* LunchPlace = (LunchPlaceInfo*)malloc(sizeof(LunchPlaceInfo));
-	
-	LunchPlace->frequency = 0;
-	LunchPlace->name = (char*)malloc(strlen(name)+1);
-	memcpy(LunchPlace->name, name, strlen(name)+1);
-	LunchPlace->desc = (char*)malloc(strlen(desc)+1);
-	memcpy(LunchPlace->desc, desc, strlen(desc)+1);
 
-	LunchPlace->pNext = gLunchPlace;
-	gLunchPlace = LunchPlace;
-	
+	LunchPlace->frequency = 0;
+	LunchPlace->name = (char*)malloc(strlen(name) + 1);
+	memcpy(LunchPlace->name, name, strlen(name) + 1);
+	LunchPlace->desc = (char*)malloc(strlen(desc) + 1);
+	memcpy(LunchPlace->desc, desc, strlen(desc) + 1);
+	LunchPlace->pNext = NULL;
+
+	printf("[add] FREQ(%d) NAME(%s) DESC(%s)\n", LunchPlace->frequency, LunchPlace->name, LunchPlace->desc);
+
+	gLunchPlaceTAIL->pNext = LunchPlace;
+	total++;
+
 	return 0;
 }
 
 int modify(int index, char* name, char* desc)
 {
+	LunchPlaceInfo* LunchPlace = gLunchPlaceHEAD;
+
+	if (0 > index || total < index)
+		return -1;
+
+	for (int i = 0; i < total; i++)
+	{
+		if (index == i)
+		{
+			free(LunchPlace->name);
+			LunchPlace->name = NULL;
+			LunchPlace->name = (char*)realloc(LunchPlace->name, strlen(name) + 1);
+			memcpy(LunchPlace->name, name, strlen(name) + 1);
+
+			free(LunchPlace->desc);
+			LunchPlace->desc = NULL;
+			LunchPlace->desc = (char*)realloc(LunchPlace->desc, strlen(desc) + 1);
+			memcpy(LunchPlace->desc, desc, strlen(desc) + 1);
+
+			printf("[modify] FREQ(%d) NAME(%s) DESC(%s)\n", LunchPlace->frequency, LunchPlace->name, LunchPlace->desc);
+			break;
+		}
+		LunchPlace = LunchPlace->pNext;
+	}
+	/*
+	printf("\n");
+	LunchPlaceInfo = gLunchPlaceHEAD;
+	while ( NULL != LunchPlaceInfo )
+	{
+	printf("[total] FREQ(%d) NAME(%s) DESC(%s)\n", LunchPlaceInfo->frequency, LunchPlaceInfo->name, LunchPlaceInfo->desc);
+	LunchPlaceInfo = LunchPlaceInfo->pNext;
+	}*/
 	return 0;
 }
 
 int remove(int index)
 {
+	LunchPlaceInfo* LunchPlace = gLunchPlaceHEAD;
+
+	if (0 > index || total < index)
+		return -1;
+
+	for (int i = 1; i < total; i++)
+	{
+		LunchPlaceInfo* targetToRemove;
+		if (index == 0)
+		{
+			targetToRemove = gLunchPlaceHEAD;
+			gLunchPlaceHEAD = gLunchPlaceHEAD->pNext;
+
+			printf("[remove] FREQ(%d) NAME(%s) DESC(%s)\n", targetToRemove->frequency, targetToRemove->name, targetToRemove->desc);
+			free(targetToRemove->name);
+			targetToRemove->name = NULL;
+			free(targetToRemove->desc);
+			targetToRemove->desc = NULL;
+			free(targetToRemove);
+			targetToRemove = NULL;
+
+			break;
+		}
+		else if (index == i)
+		{
+			targetToRemove = LunchPlace->pNext;
+			LunchPlace->pNext = targetToRemove->pNext;
+
+			printf("[remove] FREQ(%d) NAME(%s) DESC(%s)\n", targetToRemove->frequency, targetToRemove->name, targetToRemove->desc);
+			free(targetToRemove->name);
+			targetToRemove->name = NULL;
+			free(targetToRemove->desc);
+			targetToRemove->desc = NULL;
+			free(targetToRemove);
+			targetToRemove = NULL;
+
+			break;
+		}
+		LunchPlace = LunchPlace->pNext;
+	}
+
+	printf("\n");
+	LunchPlace = gLunchPlaceHEAD;
+	while (NULL != LunchPlace)
+	{
+		printf("[total] FREQ(%d) NAME(%s) DESC(%s)\n", LunchPlace->frequency, LunchPlace->name, LunchPlace->desc);
+		LunchPlace = LunchPlace->pNext;
+	}
 	return 0;
 }
 
@@ -112,19 +197,21 @@ int save()
 
 int exit()
 {
-	if ( NULL != fp) fclose(fp);
+	if (NULL != fp) fclose(fp);
 
 	printf("exit start\n");
-	while ( NULL != gLunchPlace->pNext )
+	while (NULL != gLunchPlaceHEAD->pNext)
 	{
-		LunchPlaceInfo* temp = gLunchPlace->pNext;
-		free(gLunchPlace->name);
-		free(gLunchPlace->desc);
-		free(gLunchPlace);
-		gLunchPlace = NULL;
-		gLunchPlace = temp;
+		LunchPlaceInfo* temp = gLunchPlaceHEAD->pNext;
+		free(gLunchPlaceHEAD->name);
+		gLunchPlaceHEAD->name = NULL;
+		free(gLunchPlaceHEAD->desc);
+		gLunchPlaceHEAD->desc = NULL;
+		free(gLunchPlaceHEAD);
+		gLunchPlaceHEAD = NULL;
+		gLunchPlaceHEAD = temp;
 	}
-	printf("exit done\n"); 
+	printf("exit done\n");
 
 	return 0;
 }
